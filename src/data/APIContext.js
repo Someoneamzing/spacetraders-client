0b95a0dc3d57d2cc2faeb157c58ecdc37074575c
Jacob McEwan S3841062 <s3841062@student.rmit.edu.au>
2021-08-27 01:02:03 +1000

    refactor: :loud_sound: Add Logging to APIContext
    Added simple logging to APIContext to help with debugging and monitoring

--- src/data/APIContext.js
+++ src/data/APIContext.js
@@ -6,10 +6,16 @@ export default class APIContext {
 
     async fetch(url, options = {}, withAuth = true) {
         const config = options;
-        if (!config.headers) config.headers = {};
+        config.headers = new Headers(config.headers);
         if (withAuth)
-            config.headers["Authorization"] = `Bearer ${this.accessToken}`;
-        const response = await fetch(new URL(url, this.basePath), config);
+            config.headers.set("Authorization", `Bearer ${this.accessToken}`);
+        const fullURL = new URL(url, this.basePath);
+        console.log(
+            `[${config.method || "GET"}]: ${fullURL.href}\n${[...config.headers]
+                .map(([key, value]) => `${key}: ${value}`)
+                .join("\n")}\n${config.body || ""}`
+        );
+        const response = await fetch(fullURL, config);
         if (!response.ok)
             throw new Error(
                 `${response.statusText} "${await response.text()}"`
