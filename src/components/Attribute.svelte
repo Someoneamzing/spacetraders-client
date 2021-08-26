530e0b2eaf2635c0665a8f2d0094aceb604d60c2
Jacob McEwan S3841062 <s3841062@student.rmit.edu.au>
2021-08-27 01:04:57 +1000

    refactor: :art: Add Attribute component
    Added an attribute component, a more generalised component with an icon next to text. Updated ResourceCount to use Attribute

---
+++ src/components/Attribute.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let icon;
+</script>
+
+<span class="outer">
+    <span class="material-icons">{icon}</span>
+    <slot />
+</span>
+
+<style scoped>
+    .outer {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: flex-start;
+        gap: 0.5em;
+        width: fit-content;
+    }
+</style>
--- src/components/ResourceCount.svelte
+++ src/components/ResourceCount.svelte
@@ -9,23 +9,10 @@
 </script>
 
 <script>
+    import Attribute from "./Attribute.svelte";
     import { sepThousands } from "../utils/format";
     export let amount;
     export let type;
 </script>
 
-<span class="outer">
-    <span class="material-icons">{RESOURCE_TYPES[type]}</span>
-    {!isNaN(amount) ? sepThousands(amount) : "----"}
-</span>
-
-<style scoped>
-    .outer {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: flex-start;
-        gap: 0.5em;
-        width: fit-content;
-    }
-</style>
+<Attribute icon={RESOURCE_TYPES[type]}>{sepThousands(amount)}</Attribute>
