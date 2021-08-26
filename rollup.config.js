7a4e965a78e7d713bb9effcfa352dc000b72b6b4
Jacob McEwan S3841062 <s3841062@student.rmit.edu.au>
2021-08-26 22:14:39 +1000

    chore: :heavy_plus_sign: Add rollup-plugin-node-polyfills
    Add rollup-plugin-node-polyfills to allow some basic node libs

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import livereload from "rollup-plugin-livereload";
 import replace from "@rollup/plugin-replace";
+import nodePolyfills from "rollup-plugin-node-polyfills";
 import { terser } from "rollup-plugin-terser";
 import dotenv from "dotenv";
 import css from "rollup-plugin-css-only";
@@ -43,6 +44,7 @@ export default {
         file: "public/build/bundle.js",
     },
     plugins: [
+        nodePolyfills(),
         svelte({
             compilerOptions: {
                 // enable run-time checks when not in production
